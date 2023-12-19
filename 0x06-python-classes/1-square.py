cd5fffa3fcf215c7ac28e24ffe4c22bb1de556e2
SalwaMahmoud9 <sma.mam9@gmail.com>
2023-12-19 11:38:04 +0200

    salwa 19-12-2023

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""class Square"""
+"class Square"
 
 
 class Square:
-    """class Square"""
+    "class Square"
 
     def __init__(self, size):
-        """init"""
+        "init"
         self.__size = size
