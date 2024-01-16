e3b81cd37df220b342e73c3dac8e76b2b413e633
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-01-16 13:05:00 +0200

    16-1-2024

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,12 +8,12 @@ import turtle
 
 
 class Base:
-    """base"""
+    """base."""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """init"""
+        """init."""
         if id is not None:
             self.id = id
         else:
@@ -22,14 +22,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """to_json_string"""
+        """to_json_string."""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """save_to_file"""
+        """save_to_file."""
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
             if list_objs is None:
@@ -40,14 +40,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        """from_json_string"""
+        """from_json_string."""
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """create"""
+        """create."""
         if dictionary and dictionary != {}:
             if cls.__name__ == "Rectangle":
                 new = cls(1, 1)
@@ -58,7 +58,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """load_from_file"""
+        """load_from_file."""
         filename = str(cls.__name__) + ".json"
         try:
             with open(filename, "r") as jsonfile:
@@ -69,7 +69,7 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
-        """save_to_file_csv"""
+        """save_to_file_csv."""
         filename = cls.__name__ + ".csv"
         with open(filename, "w", newline="") as csvfile:
             if list_objs is None or list_objs == []:
@@ -85,7 +85,7 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
-        """load_from_file_csv"""
+        """load_from_file_csv."""
         filename = cls.__name__ + ".csv"
         try:
             with open(filename, "r", newline="") as csvfile:
@@ -102,7 +102,7 @@ class Base:
 
     @staticmethod
     def draw(list_rectangles, list_squares):
-        """draw"""
+        """draw."""
         turt = turtle.Turtle()
         turt.screen.bgcolor("#b7312c")
         turt.pensize(3)
