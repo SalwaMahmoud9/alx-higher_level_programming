ae9cef2851f334d907e1d93e46044ef931bab69b
Salwa Mahmoud <108579776+SalwaMahmoud9@users.noreply.github.com>
2024-03-28 04:03:02 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post params
-curl -s -d "email=hr@school.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
