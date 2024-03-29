6e29221337009ef2211e85543f068a28fdf9d317
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-03-29 02:06:18 +0200

    salwa 29-3-2024

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,8 +7,8 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
+    req = requests.get(url)
+    if req.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
