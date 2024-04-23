2d097b5e4e4f4de33821d43ee9b145ea0b13c54c
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-04-23 22:52:57 +0200

    23-4-2024

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,9 +3,9 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request.get(url, function (error, response, body) {
   if (!error) {
-    const list = JSON.parse(body).characters;
+    const characters = JSON.parse(body).characters;
     const l = [];
-    list.forEach(ch => {
+    characters.forEach(ch => {
       l.push(new Promise((resolve, reject) => {
         request.get(ch, function (err, res, body) {
           if (err) {
