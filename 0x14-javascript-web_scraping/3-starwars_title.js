3fa5aa4f29837b8ab6e33b7218eb9449b71cc2be
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-04-23 23:34:56 +0200

    23-4-2024

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
+request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
   console.log(JSON.parse(body).title);
 });
