7837a7428f756d49a6ef902a5b1fcf0552e1430e
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-04-23 23:07:37 +0200

    23-4-2024

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2], function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
     let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+    
+  
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].completed === true) {
+        if (completed[todos[i].userId] === undefined) {
+          completed[todos[i].userId] = 1;
+        } else {
+          completed[ztodos[i].userId] += 1;
+        }
       }
-    });
+    }
+  
     console.log(completed);
   }
 });
